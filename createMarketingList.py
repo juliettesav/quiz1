9bcbfe7e3a78465c762e1f6c15f4176f0a64586c
juliettesav <112112053+juliettesav@users.noreply.github.com>
2022-09-20 11:54:08 -0500

    Add files via upload

---
+++ createMarketingList.py
@@ -0,0 +1,87 @@
+# Read the file VendorList.csv into this program and create a dictionary of the customer's
+# full name, email address and phone number where the key is the full name of the customer
+# and the value is a dictionary where the keys are 'email' and 'phone' and the values
+# are the corresponding email address and phone number of the customer. 
+
+
+# Once the dictionary has been completed print it out. It shoud resemble what is shown
+# below (first 2 and last 2 elements shown only):
+
+#{'Tommie Goody': {'email': 'tgoody0@weather.com', 'phone': '809-992-7298'}, 
+# 'Obadiah Godfery': {'email': 'ogodfery1@a8.net', 'phone': '560-745-9361'}......
+# ..........'Kessiah Tynemouth': {'email': 'ktynemouthdu@ning.com', 'phone': '690-215-8097'}, 
+# 'Carmela Kaubisch': {'email': 'ckaubischdv@wikia.com', 'phone': '307-726-6526'}}
+
+
+# Using the dictionary, write the contents to a csv file. The output file shoud be exactly as
+# what is shown in the file - marketinglist.csv.
+# Name your file - marketinglistFINAL.csv
+
+
+
+#________________________________________________________________________________________________#
+# Note: you can use the comments below to guide you through the logic of the code. You are not
+# required to follow it. ALSO NOT ALL STEPS HAVE BEEN COMMENTED. You may have additional steps.
+
+
+import csv
+
+# open the vendorlist file
+
+infile = 'VendorList.csv'
+
+with open(infile) as f: 
+    reader = csv.DictReader(f)
+
+    for row in reader: 
+        print(row)
+
+# create a csv object from the file object
+
+
+# create an output file
+
+
+
+
+
+# create an empty dictionary
+empty_dict = {}
+
+
+# iterate through the csv object
+
+
+
+
+    # add the key-value pair to the dictionary
+
+
+
+# print the dictionary after the loop is finished
+
+#print(myDict)
+
+
+# iternate through the dictionary and write to the output file
+
+outfile = open('marketinglistFINAL.csv','w')
+
+csvfile = csv.reader(infile,delimiter=',')
+
+next(csvfile) #to skip the header
+outfile.write("Name, Email, Phone"+'\n')
+
+for record in csvfile: 
+    
+    name=record[0]
+    email=record[1]
+    phone=record[2]
+    csvlist=[name,email,phone]
+    print(csvlist)
+    outfile.write(name+', '+email+'+'+phone+'\n')
+
+
+# close your output file
+
+outfile.close()
